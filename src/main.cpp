23ed5cdf2a4d744414cbfed290db646cb5d3a75d
Vinitvh <vineet.vh@gmail.com>
2025-04-11 21:14:40 +0530

    Read from input file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
-int main() {
-    std::cout << "Hello World!" << std::endl;
-    return 0;
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Incorrect usage. Correct usage is ..." << std::endl;
+        std::cerr << "helium <input.hl> " << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    // Buffer to store file contents
+    std::stringstream buffer;
+    // Read from the file
+    std::fstream input(argv[1], std::ios::in);
+    buffer << input.rdbuf();
+    // Convert buffer to string
+    std::string content = buffer.str();
+    input.close();
+
+    std::cout << content << std::endl;
+
+    return EXIT_SUCCESS;
 }
